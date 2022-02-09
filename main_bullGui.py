79aef4de48bebafcafb827ab465932aa5579bb43
rikard-helgegren <rikard.helgegren@gmail.com>
2022-02-09 17:35:22 +0100

    init

---
+++ main_bullGui.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    return 0
+
+
+
+
+main()
---
+++ test_main_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+from code.test_code import say_hi
+
+
+def main():
+    
+    say_hi()
+    return 0
+
+
+
+
+main()
