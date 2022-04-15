34b9796cfa7946ef32b42449cbe8c1dd0221ae4c
ByteBucket123 <elinder.david@gmail.com>
2022-04-15 13:41:44 +0200

    Fill_in_gaps_data now filters of latest_first_day and earliest_last_lastDay.
    Also duplicates the data.
    
    Also it now is MUCH faster since I stoped using numpy...
    (Because then we can run pypy!)

--- code/model/algorithms/help_algorithms.py
+++ code/model/algorithms/help_algorithms.py
@@ -1,4 +1,3 @@
-import numpy.random as nprnd
 import timeit
 
 class Node:
@@ -104,7 +103,16 @@ def linked_list_to_list(linked_list):
 ###                  Algorithms                     ###
 #######################################################
 
+def find_latest_first(lists_of_indexes):
+    return max([min(x) for x in lists_of_indexes])
+
+def find_Earliest_last(lists_of_indexes):
+    return min([max(x) for x in lists_of_indexes])
+
 def fix_gaps(lists_of_indexes):
+    return fix_gaps2(lists_of_indexes, find_latest_first(lists_of_indexes), find_Earliest_last(lists_of_indexes))
+
+def fix_gaps2(lists_of_indexes, latest_first, earliest_last):
     master_node = Node() # Dummy node
     previous_node = master_node
     current_node = None
@@ -115,8 +123,11 @@ def fix_gaps(lists_of_indexes):
         while i < len(list_of_indexes):
             index = list_of_indexes[i]
 
+            if list_of_indexes[i] < latest_first or list_of_indexes[i] > earliest_last:
+                i += 1
+
             # first node, or out of data
-            if current_node == None:
+            elif current_node == None:
                 current_node = Node(index)
                 previous_node.next = current_node
                 previous_node = current_node
@@ -161,6 +172,58 @@ def naive_fix_gaps(lists_of_indexes):
             if index not in master_list:
                 master_list.insert(find_first_index_less_than(master_list, index)+1, index)
     return master_list
+
+def my_zip(A, B):
+    i = 0
+    C = []
+    while i < len(A) and i < len(B):
+        C.append((A[i], B[i]))
+        i += 1
+    return C
+
+def fill_gaps_data(lists_to_fill, lists_of_values):
+    latest_first = find_latest_first(lists_to_fill)
+    earliest_last = find_Earliest_last(lists_to_fill)
+    master_linked_list = fix_gaps2(lists_to_fill, latest_first, earliest_last)
+    node = master_linked_list.head
+    master_lists_of_values = []
+
+    for (list_to_fill, list_of_values) in zip(lists_to_fill, lists_of_values):
+        i = 0
+        value_list = []
+        while node != None:
+
+#            print("Test>>>>>>>>>>>>>>>>>>>>>>>")
+#            print(i)
+#            print(node.data)
+#            print(list_to_fill[i])
+#            print(value_list)
+
+            if i == len(list_to_fill):
+                value_list.append(list_of_values[i-1])
+                node = node.next
+
+            # if list_to_fill[i] > earliest_last then node != None by the construction of master_linked_list
+            elif list_to_fill[i] < latest_first:
+                i += 1
+
+            elif list_to_fill[i] == node.data:
+                value_list.append(list_of_values[i])
+                node = node.next
+                i += 1
+
+            # Here we know node.data < list_to_fill[i] and we don't want to increase i
+            # Here i != 0 since then latest_first < node.data < list_to_fill[0], which is impossible
+            else:
+                value_list.append(list_of_values[i-1])
+                node = node.next
+        master_lists_of_values.append(value_list)
+        node = master_linked_list.head
+
+#    print("END<<<<<<<<<<<<<<<<<<<<<<<<<<")
+    
+    master_list = linked_list_to_list(master_linked_list)
+    return ([master_list]*len(lists_to_fill), master_lists_of_values)
 
 #######################################################
 ###                     Tests                       ###
@@ -234,12 +297,40 @@ def print_linked_list_test():
 
 def fix_gaps_test():
     list_of_lists_to_fill = [[[1, 2, 5, 6, 7, 10], [1, 2, 3, 4, 7, 8, 9]], [[1, 2, 5], [1, 2, 7, 8, 9], [1, 3, 4, 6, 7, 10]], [[1, 2], [1, 2, 7, 9], [1, 4, 6, 7, 10]]]
-    assert(linked_list_to_string(fix_gaps(list_of_lists_to_fill[0])) == '1-2-3-4-5-6-7-8-9-10-None')
-    assert(linked_list_to_string(fix_gaps(list_of_lists_to_fill[1])) == '1-2-3-4-5-6-7-8-9-10-None')
-    assert(linked_list_to_string(fix_gaps(list_of_lists_to_fill[2])) == '1-2-4-6-7-9-10-None')
+
+    assert(find_latest_first(list_of_lists_to_fill[0]) == 1)
+    assert(find_latest_first(list_of_lists_to_fill[1]) == 1)
+    assert(find_latest_first(list_of_lists_to_fill[2]) == 1)
+
+    assert(find_Earliest_last(list_of_lists_to_fill[0]) == 9)
+    assert(find_Earliest_last(list_of_lists_to_fill[1]) == 5)
+    assert(find_Earliest_last(list_of_lists_to_fill[2]) == 2)
+
+    assert(linked_list_to_string(fix_gaps(list_of_lists_to_fill[0])) == '1-2-3-4-5-6-7-8-9-None')
+    assert(linked_list_to_string(fix_gaps(list_of_lists_to_fill[1])) == '1-2-3-4-5-None')
+    assert(linked_list_to_string(fix_gaps(list_of_lists_to_fill[2])) == '1-2-None')
 
     for lists_to_fill in list_of_lists_to_fill:
-        assert(linked_list_to_list(fix_gaps(lists_to_fill)) == naive_fix_gaps(lists_to_fill))
+        assert(linked_list_to_list(fix_gaps(lists_to_fill)) == 
+               naive_fix_gaps([[i for i in list_to_fill
+                    if find_latest_first(lists_to_fill) <= i <= find_Earliest_last(lists_to_fill)] for list_to_fill in lists_to_fill]))
+
+    list_of_lists_to_fill2 = [[[1, 2, 3, 6], [1, 2, 4, 8]],
+                              [[1, 3, 5, 6, 8], [1, 3, 6, 8, 9], [1, 7, 9]],
+                              [[1, 2], [1, 2]],
+                              [[1, 2, 3, 4, 5, 6, 7, 8, 9], [1, 2, 3, 4, 5]]]
+    list_of_lists_of_data = [[[1, 2, 3, 6], [1, 2, 4, 8]],
+                             [[1, 2, 3, 4, 5], [6, 7, 8, 9, 0], [1, 2, 3]],
+                             [[1, 2], [3, 4]],
+                             [[1, 4, 2, 5, 7, 2, 7, 9, 4], [1, 5, 7, 4, 2]]]
+    list_of_expected_output = [([[1, 2, 3, 4, 6], [1, 2, 3, 4, 6]], [[1, 2, 3, 3, 6], [1, 2, 2, 4, 4]]),
+                              ([[1, 3, 5, 6, 7, 8], [1, 3, 5, 6, 7, 8], [1, 3, 5, 6, 7, 8]], [[1, 2, 3, 4, 4, 5], [6, 7, 7, 8, 8, 9], [1, 1, 1, 1, 2, 2]]),
+                              ([[1, 2], [1, 2]], [[1, 2], [3, 4]]),
+                              ([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]], [[1, 4, 2, 5, 7], [1, 5, 7, 4, 2]])]
+
+    for i in range(len(list_of_expected_output)):
+        assert(fill_gaps_data(list_of_lists_to_fill2[i], list_of_lists_of_data[i]) == 
+               list_of_expected_output[i])
 
 import random
 
@@ -272,18 +363,22 @@ def fix_gaps_preformence_test():
     print('fix gaps preformance test:')
 
     SETUP_CODE ='''
-import numpy.random as nprnd
+#import numpy.random as nprnd #Numpy does not work with pypy
+import random
 from __main__ import linked_list_to_list
 from __main__ import fix_gaps
 from __main__ import naive_fix_gaps
+from __main__ import fill_gaps_data
 s = 10 # Number of "index"
-n = 1 # Days in each "index"
-l = [nprnd.randint(n, size=n*10) for i in range(s)]
+n = 10000 # Days in each "index"
+#l = [nprnd.randint(n, size=n*10) for i in range(s)] #Numpy does not work with pypy
+l = [random.sample(range(1, n*10), n) for i in range(s)]
 '''
 
     TEST_CODE1 = 'fix_gaps(l)'
     TEST_CODE2 = 'linked_list_to_list(fix_gaps(l))'
     TEST_CODE3 = 'naive_fix_gaps(l)'
+    TEST_CODE4 = 'fill_gaps_data(l, l)'
     print('Ordinary fix_gaps:')
     print(min(timeit.repeat(setup = SETUP_CODE,
                         stmt = TEST_CODE1,
@@ -295,9 +390,14 @@ l = [nprnd.randint(n, size=n*10) for i in range(s)]
                         stmt = TEST_CODE2,
                         repeat = 100,
                         number = 1)))
-    print('Naive fix_gaps:')
+#    print('Naive fix_gaps:')
+#    print(min(timeit.repeat(setup = SETUP_CODE,
+#                        stmt = TEST_CODE3,
+#                        repeat = 100,
+#                        number = 1)))
+    print('Full fill_gaps:')
     print(min(timeit.repeat(setup = SETUP_CODE,
-                        stmt = TEST_CODE3,
+                        stmt = TEST_CODE4,
                         repeat = 100,
                         number = 1)))
 
@@ -313,6 +413,7 @@ def tests():
     fix_gaps_test()
 
     #preformence_tests()
+    fix_gaps_preformence_test()
 
 if __name__ == '__main__':
     tests()
